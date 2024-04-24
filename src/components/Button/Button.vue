00a7cbf0a0f23e2ec129a5235ce4922bfe31749c
Amin Ansari Mehr <aminansarimehrgeek23@gmail.com>
2024-04-25 02:22:40 +0330

    feat: create button component

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,8 @@
+<template>
+    <button class="loginPage__userDataSubmitBtn GlassMorphism__Button GlassMorphism__Button--Color1" id="userDataSubmitBtn">
+        <span class="GlassMorphism__LightSpan"></span>
+    </button>
+</template>
+
+
+<style src="./Button.css"></style>
